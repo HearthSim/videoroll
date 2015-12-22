5a0deb227019b712feb37de189204774f66023ec
Simon Pinfold <simon@uint8.me>
2015-12-23 00:01:14 +1300

    Initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import configparser, json, ast
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def parse_str_list(str_list):
+	if not str_list:
+		return None
+	elif "," not in str_list:
+		# only a single item in the list, so literal_eval will give a str
+		return (ast.literal_eval(str_list), )
+	else:
+		return ast.literal_eval(str_list)
+
+config = configparser.ConfigParser()
+config.read("data/hearthstone-science.ini")
+videos = [
+	(name, {
+		"url"   : config[name].get("url", "???"),
+		"build" : config[name].get("build", "Missing"),
+		"logs"  : parse_str_list(config[name].get("logs")),
+		"cards" : parse_str_list(config[name].get("cards")),
+		"tags"  : parse_str_list(config[name].get("tags")),
+	}) for name in config.sections()
+]
+
+
+@app.route("/")
+def video_index():
+	return render_template("index.html", videos=videos)
+
+
+if __name__ == "__main__":
+	app.debug = False
+	app.run()
