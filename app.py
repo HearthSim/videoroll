403f7f496f5694f5979fd50a5f3aaccd2f639085
Jerome Leclanche <jerome@leclan.ch>
2015-12-23 06:33:34 +0200

    Clean up and add primitive Github hooks

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
-
-import configparser, json, ast
-from flask import Flask, render_template
-
-import settings
+import ast
+import configparser
+import os
+import subprocess
+from flask import Flask, abort, current_app, render_template, request
 
 app = Flask(__name__)
+app.config.from_pyfile(os.path.join(os.path.dirname(__file__), "settings.cfg"))
+
 
 def parse_str_list(str_list):
 	if not str_list:
@@ -16,25 +18,66 @@ def parse_str_list(str_list):
 	else:
 		return ast.literal_eval(str_list)
 
-config = configparser.ConfigParser()
-config.read("data/hearthstone-science.ini")
-videos = [
-	(name, {
-		"url": config[name].get("url", "???"),
-		"logs": config[name].get("logs", "").split(" "),
-		"tags": config[name].get("tags", "").split(","),
-		"build": config[name].get("build", "Missing"),
-		"cards": parse_str_list(config[name].get("cards")),
-		"issues": config[name].get("issues", "").split(" "),
-	}) for name in config.sections()
-]
+
+def get_videos():
+	config = configparser.ConfigParser()
+	config.read("data/hearthstone-science.ini")
+	return [
+		(name, {
+			"url": config[name].get("url", "???"),
+			"logs": config[name].get("logs", "").split(" "),
+			"tags": config[name].get("tags", "").split(","),
+			"build": config[name].get("build", "Missing"),
+			"cards": parse_str_list(config[name].get("cards")),
+			"issues": config[name].get("issues", "").split(" "),
+		}) for name in config.sections()
+	]
+
+
+def validate_github_signature(data, signature):
+	import hmac
+	from hashlib import sha1
+	if not signature.startswith("sha1="):
+		return False
+	signature = signature.split("=")[1]
+	secret = current_app.config["GITHUB_HOOKS_SECRET"].encode("utf-8")
+	digest = hmac.new(secret, data, digestmod=sha1).hexdigest()
+	return digest == signature
 
 
 @app.route("/")
 def video_index():
-	return render_template("index.html", videos=videos, youtube_api_key=settings.youtube_api_key)
+	context = {
+		"videos": get_videos(),
+		"api_key": current_app.config["YOUTUBE_API_KEY"],
+	}
+	return render_template("index.html", **context)
+
+
+def _call(args):
+	proc = subprocess.Popen(
+		args,
+		stdin=subprocess.PIPE,
+		stdout=subprocess.PIPE,
+		stderr=subprocess.PIPE
+	)
+	stdout, stderr = proc.communicate()
+	return "Command: %r\nSTDOUT:\n%s\nSTDERR:\n%s\n" % (args, stdout, stderr)
+
+
+@app.route("/github-hooks/", methods=["POST"])
+def on_hook_trigger():
+	signature = request.headers.get("X-Hub-Signature", "")
+	import q; q("validating signature", request.data, signature)
+	if validate_github_signature(request.data, signature):
+		data = request.get_json()
+		repo = app.config["GITHUB_HOOKS_GIT_REPO"] % data["repository"]
+		ret = _call(["git", "--git-dir=%s" % (repo), "fetch", data["repository"]["clone_url"]])
+		ret += _call(["git", "--git-dir=%s" % (repo), "reset", "--hard", "FETCH_HEAD"])
+		return ret
+	abort(401)
 
 
 if __name__ == "__main__":
-	app.debug = False
+	app.debug = True
 	app.run(host="0.0.0.0", port=7070)
--- settings.sample.py
+++
@@ -1,2 +0,0 @@
-# YouTube Data API v3 API key. Ensure that this key only allows the correct referrer when using in production
-youtube_api_key = "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
